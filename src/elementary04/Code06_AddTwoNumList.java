d96d08fef02a6f30090ee7bcba166f900d68ebb7
Juli-G-81 <78062651+Juli-G-81@users.noreply.github.com>
2023-02-05 23:22:54 +0800

    2, 21

--- src/elementary04/Code06_AddTwoNumList.java
+++ src/elementary04/Code06_AddTwoNumList.java
@@ -6,6 +6,10 @@ import class04.Code05_AddTwoNumbers;
  * @author ：Juli
  * @date ： 2023/2/5 9:29 PM
  * @description：两个链表对应位置数值相加
+ * 给定两个链表的头节点head1和head2. 认为从左到右是某个数字从低位到高位，返回相加之后的链表
+ * 例子 4 ->3 ->6 2 -> 5 -> 3
+ * 返回 6->8->9
+ * 解释 634 +352 = 986
  * @modifiedBy ：
  * @version: 1.0
  */
@@ -80,6 +84,5 @@ public class Code06_AddTwoNumList {
             System.out.println(addTwoListNums.val);
             addTwoListNums = addTwoListNums.next;
         }
-
     }
 }
